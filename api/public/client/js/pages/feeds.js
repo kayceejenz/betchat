4017685945f06744ab77d66c5607aa0b1196e582
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-03-12 22:06:17 +0100

    Update client

--- api/public/client/js/pages/feeds.js
+++ api/public/client/js/pages/feeds.js
@@ -41,22 +41,22 @@ getFeeds = () =>{
                 $.each(response.data,(i,feed) => {
                     const  template = `
                     <div class="col-md-12">
-                        <div class="feed p-2">
-                            <div class="bg-white border mt-2">
-                                <div>
-                                    <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-                                        <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="${feed.image}" width="45">
-                                            <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">Thomson ben</span><span class="text-black-50 time">40 minutes ago</span></div>
-                                        </div>
-                                        <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
+                    <div class="feed p-2">
+                        <div class="bg-white border mt-2">
+                            <div>
+                                <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
+                                    <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="${response.data.postedBy.image}" width="45">
+                                        <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">${response.data.postedBy.fullname}</span><span class="text-black-50 time">${new Date(response.data.createdAt).toLocaleDateString()} ${new Date(response.data.createdAt).toLocaleTimeString()}</span></div>
                                     </div>
+                                    <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
                                 </div>
-                                <div class="p-2 px-3"><h5>${feed.title}</h5></div>
-                                <div class="p-2 px-3"><span>${feed.body}</span></div>
-                                <div class="d-flex justify-content-end socials p-2 py-3"><i class="fa fa-thumbs-up"></i><i class="fa fa-comments-o"></i><i class="fa fa-share"></i></div>
                             </div>
+                            <div class="p-2 px-3"><h5>${response.data.title}</h5></div>
+                            <div class="p-2 px-3"><span>${response.data.body}</span></div>
+                            <div class="d-flex justify-content-end socials p-2 py-3"><i class="fa fa-thumbs-up"></i><i class="fa fa-comments-o"></i><i class="fa fa-share"></i></div>
                         </div>
                     </div>
+                  </div>
                     `
                     $("#content").append(template)
                 })
@@ -108,7 +108,7 @@ getFeeds = () =>{
                                     <div class="bg-white border mt-2">
                                         <div>
                                             <div class="d-flex flex-row justify-content-between align-items-center p-2 border-bottom">
-                                                <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="${response.data.image}" width="45">
+                                                <div class="d-flex flex-row align-items-center feed-text px-2"><img class="rounded-circle" src="${response.data.postedBy.image}" width="45">
                                                     <div class="d-flex flex-column flex-wrap ml-2"><span class="font-weight-bold">${response.data.postedBy.fullname}</span><span class="text-black-50 time">${new Date(response.data.createdAt).toLocaleDateString()} ${new Date(response.data.createdAt).toLocaleTimeString()}</span></div>
                                                 </div>
                                                 <div class="feed-icon px-2"><i class="fa fa-ellipsis-v text-black-50"></i></div>
