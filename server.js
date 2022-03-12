94d0e863f12a63d0a4f12efb669599741bc68081
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-03-12 16:31:42 +0100

    Serving

--- server.js
+++ server.js
@@ -10,9 +10,29 @@ require("./api/src/middlewares/pre-route.middleware")(app);
 // API routes
 app.use("/api", require("./api/src/routes"));
 
-// Ping route for testing connection
-app.get("/ping", (req, res) => res.status(200).send("Hello world!"));
+// client routes
 app.get("/auth/sign-in", (req,res) => res.sendFile(__dirname+"/api/public/client/pages/auth/sign-in.html"));
+app.get("/auth/sign-up", (req,res) => res.sendFile(__dirname+"/api/public/client/pages/auth/sign-up.html"));
+app.get("/feeds/index", (req,res) => res.sendFile(__dirname+"/api/public/client/pages/feeds/feeds.html"));
+
+// images
+app.get("/images/logo", (req,res) => res.sendFile(__dirname+"/api/public/client/images/logo.png"));
+
+// css
+app.get("/css/bootstrap", (req,res) => res.sendFile(__dirname+"/api/public/client/css/dist/bootstrap.min.css"));
+app.get("/css/toastr", (req,res) => res.sendFile(__dirname+"/api/public/client/css/dist/toastr.min.css"));
+app.get("/css/bootstrap-tagsinput", (req,res) => res.sendFile(__dirname+"/api/public/client/css/dist/bootstrap-tagsinput.css"));
+app.get("/css/signin", (req,res) => res.sendFile(__dirname+"/api/public/client/css/pages/sign-in.css"));
+
+
+// js
+app.get("/js/jquery", (req,res) => res.sendFile(__dirname+"/api/public/client/js/libs/jquery.min.js"));
+app.get("/js/toastr", (req,res) => res.sendFile(__dirname+"/api/public/client/js/libs/toastr.min.js"));
+app.get("/js/bootstrap-tagsinput", (req,res) => res.sendFile(__dirname+"/api/public/client/js/libs/bootstrap-tagsinput.min.js"));
+app.get("/js/signin", (req,res) => res.sendFile(__dirname+"/api/public/client/js/pages/signin.js"));
+app.get("/js/signup", (req,res) => res.sendFile(__dirname+"/api/public/client/js/pages/signup.js"));
+
+
 
 // Serve swagger ui
 // app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
