afa386625b05ac615bf4ff9196fbcc659bc407f2
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-03-12 16:14:58 +0100

    serve

--- api/public/client/js/pages/sign-in.js
+++ api/public/client/js/pages/sign-in.js
@@ -1,14 +1,28 @@
+validate = (input) => {
+    if(input.value === ""){
+        input.classList.add("is-invalid")
+    }
+    else{
+        input.classList.remove("is-invalid")
+    }
+}
+
 
 $("#loginbtn").on("click",(e)=> {
     e.preventDefault()
-    e.target.innerText = "Authenticating..."
+    let form = $("form")[0]
+    let inputs = $("form input");
+    $.each(inputs,(i,input) => {
+        validate(input)
+    })
 
-    const email = $("#email").val();
-    const password = $("#password").val();
-    if(email === "" && password === "" ){
-        alert("email and password required")
-    }
-    else{
+
+    if(form.checkValidity()){
+        e.target.innerText = "Authenticating..."
+
+        const email = $("#email").val()
+        const password = $("#password").val()
+
         $.ajax({
             url:"https://betchat-backend-test.herokuapp.com/api/auth/sign-in",
             type: "POST",
@@ -17,18 +31,18 @@ $("#loginbtn").on("click",(e)=> {
             data: JSON.stringify({ email: email, password: password }),
             success: response => {
                 if(response.success){
-                    alert("Login success")
+                    toastr.success("Login successful.")
                 }
                 else{
-                    alert("Login failed")
+                    toastr.error("Login successful.")
                 }
                 e.target.innerText = "Sign in"
             },
             error: err => {
-                alert("Auth failed");
+                toastr.error("Auth failed")
                 e.target.innerText = "Sign in"
             },
         })
     }
- 
+     
 });
