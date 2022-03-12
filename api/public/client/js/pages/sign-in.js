8d339c69d4b71451c16b958b1b6e42ac09cc71a3
Precious Kosisochukwu <PreciousKosisochukwu01@gmail.com>
2022-03-12 17:53:54 +0100

    route redirections

--- api/public/client/js/pages/sign-in.js
+++ api/public/client/js/pages/sign-in.js
@@ -9,6 +9,10 @@ validate = (input) => {
     }
 }
 
+passValidate = (status) => {
+    return status ? true : false
+}
+
 
 $("#loginbtn").on("click",(e)=> {
     e.preventDefault()
@@ -19,7 +23,7 @@ $("#loginbtn").on("click",(e)=> {
     $.each(inputs,(i,input) => {
         validStates.push(validate(input))
     })
-    isAllValid = validStates.every((i,state) => { return state ? true : false })
+    isAllValid = validStates.every(passValidate)
 
 
     if(isAllValid){
